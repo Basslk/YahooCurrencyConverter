50fd90c4d8710e8f7fdf4355ab3cffcf94e9f3cd
Bassl Kokash <basslk@hotmail.com>
2012-10-08 01:07:42 +0300

    initial

---
+++ BKSystems.Finance/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BKSystems.Finance.CurrencyConverter")]
+[assembly: AssemblyDescription("An on-line Currency Converter Class")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("formely BKSystems, now Lite-Soft ")]
+[assembly: AssemblyProduct("BKSystems.Finance.CurrencyConverter")]
+[assembly: AssemblyCopyright("non")]
+[assembly: AssemblyTrademark("BKSystems.Finance")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5749721-c0c4-4060-922c-0d42251ae8d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
